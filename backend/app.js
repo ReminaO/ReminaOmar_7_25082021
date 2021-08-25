1925d0e40684765d8c80f1d52a9b33c93d7c027d
ReminaO <oremina@hotmail.fr>
2021-08-25 20:15:33 +0200

    ajout du middleware multer

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const userRoutes = require('./routes/user');
 
@@ -14,6 +15,8 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
   
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use('/api/auth', userRoutes);
 
 module.exports = app;

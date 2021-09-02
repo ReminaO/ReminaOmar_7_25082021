35a6989e78b84e970043f8a261a6a2e0d203b9b4
ReminaO <oremina@hotmail.fr>
2021-09-02 22:32:57 +0200

    Update comments.js

---
+++ backend/routes/comments.js
@@ -0,0 +1,18 @@
+//import des modules
+const express = require('express');
+const router = express.Router();
+
+// Middleware pour l'autentification
+const auth = require('../middleware/auth');
+
+//import du controller
+const commentsCtrl = require('../controllers/comments');
+
+//Routes pour appeler les controllers messages
+router.post('/', auth, commentsCtrl.createComment);
+router.put('/:id', auth, commentsCtrl.modifyComment)
+router.delete('/:id/', auth, commentsCtrl.deleteComment);
+router.get('/', auth, commentsCtrl.getAllComments);
+
+
+module.exports = router;//Export du fichier
